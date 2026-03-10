72a6f578a841046c4d969f7b76d42f73e59d6eb1
Allen <stuallen@gmail.com>
2026-03-11 00:24:21 +0800

    feat: 熱更新取得最新instance

--- src/injected/index.ts
+++ src/injected/index.ts
@@ -1,30 +1,36 @@
-import { traverse } from './walker'
-import type { ExtendedComponentInstance } from '../types/vue-internals'
-import { getGraph, setOnUpdate } from '../types/graph'
-import type { NodeType } from '../types/graph'
+import { traverse } from "./walker";
+import type { ExtendedComponentInstance } from "../types/vue-internals";
+import { getGraph, setOnUpdate } from "../types/graph";
+import type { NodeType } from "../types/graph";
 
 interface VueAppInternals {
   __vue_app__?: {
-    _instance: ExtendedComponentInstance | null
-  }
+    _instance: ExtendedComponentInstance | null;
+  };
 }
 
+const appEl = document.querySelector("#app") as
+  | (Element & VueAppInternals)
+  | null;
+const app = appEl?.__vue_app__?._instance;
+const hmr = (window as any).__VUE_HMR_RUNTIME__;
 
-
-const appEl = document.querySelector('#app') as (Element & VueAppInternals) | null
-const app = appEl?.__vue_app__?._instance
-const hmr = (window as any).__VUE_HMR_RUNTIME__
-
+const hook = (window as any).__VUE_DEVTOOLS_GLOBAL_HOOK__;
+const originalEmit = hook.emit.bind(hook);
+let pendingReloadId: string | null = null;
 
 if (app) {
-  const graph = getGraph()
+  const graph = getGraph();
 
   function sanitizeVal(val: unknown, type: NodeType): unknown {
     switch (type) {
-      case 'ref':
-      case 'computed': return (val as any)?._value;
-      case 'reactive': return { ...(val as object) }
-      case 'watch':    return ''
+      case "ref":
+      case "computed":
+        return (val as any)?._value;
+      case "reactive":
+        return { ...(val as object) };
+      case "watch":
+        return "";
     }
   }
 
@@ -32,54 +38,45 @@ if (app) {
     const plain = Object.fromEntries(
       Object.entries(graph).map(([comp, nodes]) => [
         comp,
-        nodes.map(n => ({
-          id:   n.id,
+        nodes.map((n) => ({
+          id: n.id,
           type: n.type,
           file: n.file,
-          val:  sanitizeVal(n.val, n.type),
+          val: sanitizeVal(n.val, n.type),
           deps: n.deps,
           subs: n.subs,
         })),
-      ])
-    )
+      ]),
+    );
     // console.log('plain', plain)
-    ;(window as unknown as Record<string, unknown>).__vueReactivityGraph = plain
+    (window as unknown as Record<string, unknown>).__vueReactivityGraph = plain;
     // window.postMessage({ type: 'VUE_GRAPH_UPDATE' }, '*')
-    window.postMessage({type: 'VUE_GRAPH_UPDATE' },'*')
+    window.postMessage({ type: "VUE_GRAPH_UPDATE" }, "*");
   }
 
-  
   if (hmr) {
-    const originalReload = hmr.reload
-    const originalRerender = hmr.rerender
+    const originalReload = hmr.reload;
 
-    hmr.reload = function(id: string, newComp: any) {
-      originalReload.call(this, id, newComp)
-      // originalReload 內部用 queueJob 排程，是非同步的
-      // 必須等 Vue scheduler flush 後才能拿到新的 instance
-      setTimeout(() => {
-        const freshApp = appEl?.__vue_app__?._instance
-        if (!freshApp) return
-        console.log('freshApp', freshApp)
-        const g = getGraph()
-        Object.keys(g).forEach(k => delete g[k])
-        traverse(freshApp)
-        refreshGraph()
-      }, 0)  // 0ms 就夠，因為 Vue scheduler 在 microtask 跑
-    }
-
-
-    // hmr.rerender = function(id: string, newRender: any) {
-    //   console.log('hmr rerender', id)
-    // //   originalRerender.call(this, id, newRender)
-    // //   setTimeout(() => {
-    // //     traverse(app)
-    // //     // refreshGraph()
-    // //   }, 50)
-    // }
+    hmr.reload = function (id: string, newComp: any) {
+      pendingReloadId = id;
+      return originalReload.call(this, id, newComp);
+    };
   }
 
-  setOnUpdate(refreshGraph)
-  traverse(app)
-  refreshGraph()
+  hook.emit = function (event: string, ...args: any[]) {
+    if (event === "component:added" && pendingReloadId !== null) {
+      const [app, uid, parentUid, instance] = args;
+      // 比對 __hmrId 確認是同一個 component
+      if (instance?.type?.__hmrId === pendingReloadId) {
+        traverse(instance);
+        refreshGraph();
+        pendingReloadId = null;
+      }
+    }
+    return originalEmit(event, ...args);
+  };
+
+  setOnUpdate(refreshGraph);
+  traverse(app);
+  refreshGraph();
 }
